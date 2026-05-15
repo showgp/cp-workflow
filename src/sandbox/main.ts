f8ac79d8bee17fb9b83c111d3297c4e01bac7f65
Rui Peng <rui.peng@fenrir-inc.com.cn>
2026-05-15 11:54:28 +0800

    chore: increase plugin window height to 600px to reduce scrolling

--- src/sandbox/main.ts
+++ src/sandbox/main.ts
@@ -1,6 +1,6 @@
 import { messageHandler } from './message-handler';
 
-figma.showUI(__html__);
+figma.showUI(__html__, { width: 360, height: 600 });
 
 figma.ui.onmessage = (msg: unknown) => {
   messageHandler(msg);
